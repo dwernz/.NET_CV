08c6dfbd3de8239e0a1837124c45bd4b27407976
Daniel <62781672+dwernz@users.noreply.github.com>
2022-12-15 10:07:24 -0500

    Home page update and added recipes

---
+++ CV/Pages/Recipes/CitrusTarragonButter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CV.Pages.Recipes
+{
+    public class CitrusTarragonButterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
