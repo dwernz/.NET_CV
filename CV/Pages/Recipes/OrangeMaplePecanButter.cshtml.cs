92f887728f8154ad7ac942d54f2745f9224d22f4
Daniel <62781672+dwernz@users.noreply.github.com>
2022-12-18 13:13:29 -0500

    Added Recipes
    Added Recipes and made some rearrangements on the Portfolio page.

---
+++ CV/Pages/Recipes/OrangeMaplePecanButter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CV.Pages.Recipes
+{
+    public class OrangeMaplePecanButterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CV/Pages/Recipes/SpicedBrownSugarButter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CV.Pages.Recipes
+{
+    public class SpicedBrownSugarButterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
