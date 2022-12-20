166fdf51760a84d5931a75c9d61aa2fe104e8950
Daniel <62781672+dwernz@users.noreply.github.com>
2022-12-19 19:13:50 -0500

    Added Recipes and Favicon

---
+++ CV/Pages/Recipes/French75Mocktail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CV.Pages.Recipes
+{
+    public class French75MocktailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CV/Pages/Recipes/PickledCurryMangoes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CV.Pages.Recipes
+{
+    public class PickledCurryMangoesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
